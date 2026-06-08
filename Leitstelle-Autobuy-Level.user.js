cec2f21293916653f2274bf0bca4f1354ea4094e
NilsPee <126229632+NilsPee@users.noreply.github.com>
2026-06-08 08:06:48 +0200

    Update Leitstelle-Autobuy-Level.user.js

--- Leitstelle-Autobuy-Level.user.js
+++ Leitstelle-Autobuy-Level.user.js
@@ -13,7 +13,7 @@
 // @match        https://*.leitstellenspiel.de/settings/index*
 // @grant        GM.getValue
 // @grant        GM.setValue
-// @require      https://raw.githubusercontent.com/NilsPee/LSS_V2_Scripts/main/NilsPe-Skriptbasis.user.js?v=1.0.1
+// @require      https://raw.githubusercontent.com/NilsPee/LSS_V2_Scripts/main/NilsPe-Skriptbasis.user.js?v=1.0.2
 // @run-at       document-idle
 // ==/UserScript==
 
